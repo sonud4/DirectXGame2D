90d2b57abb71d0630559914a35bc60d98455efc8
sonud4 <gn.sonuda@gmail.com>
2021-11-09 17:40:41 +0900

    ウィンドウ作成

---
+++ 2DGame/2DGame/main.cpp
@@ -0,0 +1,40 @@
+#include<Windows.h>
+#include<stdio.h>
+
+#pragma comment(lib,"winmm.lib")
+
+//プロトタイプ宣言
+LRESULT CALLBACK WndProc(
+	HWND   hwnd,
+	UINT   message,
+	WPARAM wParam,
+	LPARAM lParam
+);
+
+//エントリポイント
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	//***変数宣言***
+	//ウィンドウクラス情報を設定する構造体
+	WNDCLASSEX wcex;
+	//作成したウィンドウのハンドル
+	HWND	   hWnd;
+	MSG		   messsage;
+
+	//***ウィンドウクラス情報の設定***
+	ZeroMemory(&wcex, sizeof(wcex));
+	//作成するウィンドウと紐づけ
+	wcex.hInstance = hInstance;
+	//ウィンドウクラス情報を識別する名前
+	wcex.lpszClassName = "Class Name";
+	//ウィンドウプロシージャの指定
+	wcex.lpfnWndProc = WndProc;
+	//ウィンドウのスタイル指定
+	wcex.style = CS_CLASSDC;
+	//
+}
